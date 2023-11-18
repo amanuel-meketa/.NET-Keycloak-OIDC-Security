21500e343f95508d738648e61307e176400c55c4
Amanuel <Amanuel.Eshete1@gmail.com>
2023-11-18 09:54:55 +0300

    #changed the api layer to mvc presentation layer - OIDC configured

---
+++ DotNet-Keycloak/DotNet-Keycloak.presentation/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie(cookie =>
+    {
+        cookie.Cookie.Name = "keycloak.cookie";
+        cookie.Cookie.MaxAge = TimeSpan.FromMinutes(60);
+        cookie.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+        cookie.SlidingExpiration = true;
+    })
+    .AddOpenIdConnect(options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.Authority = builder.Configuration.GetSection("Keycloak")["ServerRealm"];
+        options.ClientId = builder.Configuration.GetSection("Keycloak")["ClientId"];
+        options.ClientSecret = builder.Configuration.GetSection("Keycloak")["ClientSecret"];
+        options.MetadataAddress = builder.Configuration.GetSection("Keycloak")["Metadata"];
+        options.RequireHttpsMetadata = false;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.SaveTokens = true;
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.NonceCookie.SameSite = SameSiteMode.Unspecified;
+        options.CorrelationCookie.SameSite = SameSiteMode.Unspecified;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "roles",
+            RoleClaimType = ClaimTypes.Role,
+            ValidateIssuer = true
+        };
+
+    });
+
+builder.Services.AddAuthorization();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
